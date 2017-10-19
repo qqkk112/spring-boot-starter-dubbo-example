e0a9d19bff729e5a0a765ef382fe68ef644dc787
雷 <619137943@qq.com>
2017-10-19 22:35:24 +0800

    升级依赖至1.0.6

--- example-provider/src/main/java/com/test/dubbo/main/SpringDubboxConfigApplication.java
+++ example-provider/src/main/java/com/test/dubbo/main/SpringDubboxConfigApplication.java
@@ -1,23 +1,33 @@
 package com.test.dubbo.main;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication 
-public class SpringDubboxConfigApplication implements CommandLineRunner {
-
+public class SpringDubboxConfigApplication implements InitializingBean,DisposableBean {
+	private static CountDownLatch latch=new CountDownLatch(1);
+	private static ConfigurableApplicationContext context;
+	
 	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(SpringDubboxConfigApplication.class, args);
-		TimeUnit.MINUTES.sleep(10); //发布者暂停10分钟等待被调用
-		System.err.println("服务提供者等待结束------>>服务关闭");
+		context = SpringApplication.run(SpringDubboxConfigApplication.class, args);
+		latch.await();
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void afterPropertiesSet() throws Exception {
 		System.err.println("服务提供者启动完毕------>>启动完毕");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		latch.countDown();
+		context.close();
+		System.err.println("服务提供者等待结束------>>服务关闭");
 	}
 	
 }
