9dde111770136484134f26b108084ad85d859d3c
雷 <619137943@qq.com>
2017-11-21 23:31:41 +0800

    "

--- example-provider/src/main/java/com/reger/test/core/DubboLeaderApplication.java
+++ example-provider/src/main/java/com/reger/test/core/DubboLeaderApplication.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CountDownLatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,8 +13,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.reger.dubbo.rpc.filter.Utils;
 import com.reger.test.exception.TestRuntimeException;
 
-@SpringBootApplication
-public class DubboLeaderApplication implements InitializingBean, DisposableBean {
+@SpringBootApplication(scanBasePackages="com.reger.test.provider")
+public class DubboLeaderApplication implements CommandLineRunner, DisposableBean {
 
 	private  final Logger log = LoggerFactory.getLogger(DubboLeaderApplication.class);
 
@@ -22,14 +22,10 @@ public class DubboLeaderApplication implements InitializingBean, DisposableBean 
 	private static ConfigurableApplicationContext context;
 
 	public static void main(String[] args) throws InterruptedException {
+		Utils.register(TestRuntimeException.class); // 注册允许传递的异常类型
+		Utils.register(RuntimeException.class); // 注册允许传递的异常类型
 		context = SpringApplication.run(DubboLeaderApplication.class, args);
 		latch.await();
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		Utils.register(TestRuntimeException.class); // 注册允许传递的异常类型
-		log.info("服务提供者启动完毕------>>启动完毕");
 	}
 
 	@Override
@@ -37,6 +33,11 @@ public class DubboLeaderApplication implements InitializingBean, DisposableBean 
 		latch.countDown();
 		context.close();
 		log.info("服务提供者关闭------>>服务关闭");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("服务提供者启动完毕------>>启动完毕");
 	}
 
 }
