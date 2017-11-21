4f94295a0e445eb92f909d1a19e6cefc11e286f8
雷 <619137943@qq.com>
2017-11-21 15:49:33 +0800

    “

--- example-consumer/src/main/java/com/reger/test/core/DubboLeaderApplication.java
+++ example-consumer/src/main/java/com/reger/test/core/DubboLeaderApplication.java
@@ -27,21 +27,21 @@ public class DubboLeaderApplication implements CommandLineRunner {
 	@Bean
 	public ConsumerFilter consumerFilter1() {
 		return (joinPoint) -> {
-			log.info("1.调用接口 ------》》" + joinPoint.getInvoker().getInterface());
+			log.info("1.调用接口 ------》》" + joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
 	@Bean
 	public ConsumerFilter consumerFilter2() {
 		return (joinPoint) -> {
-			log.info("2.调用接口 ------》》" + joinPoint.getInvoker().getInterface());
+			log.info("2.调用接口 ------》》" + joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
 	@Bean
 	public ConsumerFilter consumerFilter3() {
 		return (joinPoint) -> {
-			log.info("3.调用接口 ------》》" + joinPoint.getInvoker().getInterface());
+			log.info("3.调用接口 ------》》" + joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
--- example-provider/src/main/java/com/reger/test/core/DubboLeaderApplication.java
+++ example-provider/src/main/java/com/reger/test/core/DubboLeaderApplication.java
@@ -44,21 +44,21 @@ public class DubboLeaderApplication implements InitializingBean, DisposableBean 
 	@Bean
 	public ProviderFilter providerFilter1() {
 		return (joinPoint) -> {
-			log.info("1.方法{}被调用 ", joinPoint.getInvoker().getInterface());
+			log.info("1.方法{}被调用 ", joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
 	@Bean
 	public ProviderFilter providerFilter2() {
 		return (joinPoint) -> {
-			log.info("2.方法{}被调用 ", joinPoint.getInvoker().getInterface());
+			log.info("2.方法{}被调用 ", joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
 	@Bean
 	public ProviderFilter providerFilter3() {
 		return (joinPoint) -> {
-			log.info("3.方法{}被调用 ", joinPoint.getInvoker().getInterface());
+			log.info("3.方法{}被调用 ", joinPoint.getInterface());
 			return joinPoint.proceed();
 		};
 	}
